1f67f6656385062bd337dc6b59782ac2e67f3d45
Michał Pocheć (michpoc547) <michpoc547@student.polsl.pl>
2019-04-13 11:17:37 +0200

    Pierwsze działające coś

--- app/src/main/java/com/example/talaria/MainActivity.java
+++ app/src/main/java/com/example/talaria/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.talaria;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button bMap, bClear, bPosition;
+    TextView tvText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bMap = (Button) findViewById(R.id.bShowMap);
+        bClear = (Button) findViewById(R.id.bClear);
+        bPosition = (Button) findViewById(R.id.bGetPosition);
+        tvText = (TextView) findViewById(R.id.tvText);
+
+        bClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvText.setText("");
+            }
+        });
+
+        bPosition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvText.setText("Gówno w dupie a nie position");
+            }
+        });
     }
 }
