74af28743a84ae56f21c5f6ea32a65a8a19ae822
Kyeiv <krzy.poloczek@gmail.com>
2019-04-13 15:27:54 +0200

    Added your location marker

--- app/src/main/java/com/example/talaria/GPSTracker.java
+++ app/src/main/java/com/example/talaria/GPSTracker.java
@@ -1,13 +1,16 @@
 package com.example.talaria;
 
+import android.Manifest;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v4.content.ContextCompat;
 
 public class GPSTracker extends Service implements LocationListener {
 
@@ -22,7 +25,27 @@ public class GPSTracker extends Service implements LocationListener {
     public Location getLocation() {
         locationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
         isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-       //isNetworkEnabled = locationManager.
+        isNetworkEnabled = locationManager.isProviderEnabled(locationManager.NETWORK_PROVIDER);
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            if (isGPSEnabled) {
+                if(location == null){
+                    locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 1_000, 5, this);
+                    if(locationManager != null){
+                        location = locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
+                    }
+                }
+            }
+            if (location == null) {
+                if(isNetworkEnabled){
+                    locationManager.requestLocationUpdates(locationManager.NETWORK_PROVIDER, 1_000, 5, this);
+                    if(locationManager != null){
+                        location = locationManager.getLastKnownLocation(locationManager.NETWORK_PROVIDER);
+                    }
+                }
+            }
+        }
         return location;
     }
 
@@ -39,7 +62,7 @@ public class GPSTracker extends Service implements LocationListener {
 
     }
 
-   // @androidx.annotation.Nullable
+    // @androidx.annotation.Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
