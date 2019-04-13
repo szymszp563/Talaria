4ba10763692d050c5873294643de632bbc8a1383
szymon.szpakowski <szymon.szpakowski6@gmail.com>
2019-04-13 17:22:24 +0200

    Handling asking for permission class.

--- app/src/main/java/com/example/talaria/MainActivity.java
+++ app/src/main/java/com/example/talaria/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,8 +18,9 @@ public class MainActivity extends AppCompatActivity {
     Button bMap, bClear, bPosition, bClient, bServer;
     TextView tvText;
     private static final int REQUEST_LOCATION = 123;
-
-
+    //permissions
+    HandlePermission fineLocation;
+    HandlePermission coarseLocation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
         tvText = (TextView) findViewById(R.id.tvText);
         bServer = (Button) findViewById(R.id.bServer);
         bClient = (Button) findViewById(R.id.bClient);
+
+        fineLocation =
+                new HandlePermission(this, Manifest.permission.ACCESS_FINE_LOCATION, this,REQUEST_LOCATION);
+        coarseLocation =
+                new HandlePermission(this, Manifest.permission.ACCESS_COARSE_LOCATION, this,REQUEST_LOCATION);
 
         bClient.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,58 +69,56 @@ public class MainActivity extends AppCompatActivity {
         bMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               Intent mapActivityIntent = new Intent(getApplicationContext(), MapsActivity.class);
-               startActivity(mapActivityIntent);
+               if(fineLocation.getAccessGranted() || coarseLocation.getAccessGranted()){
+                   Intent mapActivityIntent = new Intent(getApplicationContext(), MapsActivity.class);
+                   startActivity(mapActivityIntent);
+               }else{
+                   Toast.makeText(getApplicationContext(), "Enable localisation!", Toast.LENGTH_SHORT).show();
+               }
+
             }
         });
 
-        // Here, thisActivity is the current activity
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
 
-            // Permission is not granted
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.ACCESS_FINE_LOCATION)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-            } else {
-                // No explanation needed; request the permission
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-        } else {
-            // Permission has already been granted
-        }
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            // Permission is not granted
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-            } else {
-                // No explanation needed; request the permission
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-        } else {
-            // Permission has already been granted
-        }
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        Toast.makeText(getApplicationContext(), "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@", Toast.LENGTH_SHORT).show();
+        switch (requestCode) {
+            case REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    for (String permission:permissions) {
+                        if(permission.equals(Manifest.permission.ACCESS_FINE_LOCATION))
+                            fineLocation.setAccessGranted(true);
+                        else if(permission.equals(Manifest.permission.ACCESS_COARSE_LOCATION))
+                            coarseLocation.setAccessGranted(true);
+
+                    }
+                    fineLocation.setAccessGranted(true);
+
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
 }
